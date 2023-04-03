c9cd8f7ec6723c80af179cba1a024cadf3f25848
Gregory Odiase <gregory.odiase@cwg-plc.com>
2023-04-03 11:24:29 +0100

    Feat: Initial commit of rebuild

---
+++ sydnyofficial/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
+
+
